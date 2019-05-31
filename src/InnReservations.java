b4efbe6d1115fa92b401a32ec8757fb1c775facb
Scott Rizzo <scottrizzo@Scotts-MacBook-Pro-2.local>
2019-05-31 09:57:28 -0700

    added controller

--- src/InnReservations.java
+++ src/InnReservations.java
@@ -5,5 +5,9 @@ public class InnReservations {
         //Going to start by planning this out tonight with possible design patterns
         //create and insert tables
         //Get connection on model
+
+        InnController controller = new InnController();
+
+        System.out.println("WE are working");
     }
 }
