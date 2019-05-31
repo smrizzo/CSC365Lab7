35a73d26296826082763935b1d96e4a8d8598062
James chavez <jameschavez@Jamess-MacBook-Air.local>
2019-05-31 12:24:34 -0700

    a basic method holding the parser

--- src/InnReservations.java
+++ src/InnReservations.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.lang.String;
 public class InnReservations {
 
     public static void main(String[] args) {
@@ -9,5 +11,56 @@ public class InnReservations {
         InnController controller = new InnController();
 
         System.out.println("WE are working");
+    }
+
+    public String parse()
+    {
+        Scanner inp = new Scanner(System.in);
+        String test = inp.nextLine();
+        String delims = " ";
+        String [] strarray = inp.split(delims);
+
+        System.out.println("MENU:\n");
+        System.out.println("(1) Rooms and Rates\n");
+        System.out.println("(2) Reservations\n");
+        System.out.println("(3) Reservation Change\n");
+        System.out.println("(4) Reservation Cancellation\n");
+        System.out.println("(5) Revenue\n");
+
+        if(strarray[0].equals("1"))
+        {
+
+
+        }else if(strarray[0].equals("2"))
+        {
+
+
+        }
+        else if(strarray[0].equals("3"))
+        {
+
+
+        }
+        else if(strarray[0].equals("4"))
+        {
+
+
+        }
+        else if(strarray[0].equals("5"))
+        {
+
+
+        }
+        else if(strarray[0].equals("6"))
+        {
+
+
+        }
+        else
+        {
+            System.out.println("invalid Input\n");
+        }
+
+
     }
 }
